0afafde694f277e7ca6ddcbd0268476bf4edaf21
Govind Sahai <gsiitbhu@gmail.com>
2016-02-24 23:26:13 +0530

    Add Exceptional Handling in FinalRoute.xaml.cs

--- client/whereAir/FinalRoute.xaml.cs
+++ client/whereAir/FinalRoute.xaml.cs
@@ -62,79 +62,101 @@ namespace whereAir
 
         private void AddJsonPoints(string json)
         {
-            Regex reg = new Regex(@"\[(?<latitude>[\.\d]+), (?<longitude>[\.\d]+)\]");
-
-            foreach (Match m in reg.Matches(json))
+            try
             {
-                double regLatitude = double.Parse(m.Groups["latitude"].Value);
-                double regLongitude = double.Parse(m.Groups["longitude"].Value);
-                Debug.WriteLine("Latitude : " + m.Groups["latitude"].Value);
-                Debug.WriteLine("Longitude : " + m.Groups["longitude"].Value);
+                Regex reg = new Regex(@"\[(?<latitude>[\.\d]+), (?<longitude>[\.\d]+)\]");
 
-                AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = regLatitude, Longitude = regLongitude }));
+                foreach (Match m in reg.Matches(json))
+                {
+                    double regLatitude = double.Parse(m.Groups["latitude"].Value);
+                    double regLongitude = double.Parse(m.Groups["longitude"].Value);
+                    Debug.WriteLine("Latitude : " + m.Groups["latitude"].Value);
+                    Debug.WriteLine("Longitude : " + m.Groups["longitude"].Value);
+
+                    AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = regLatitude, Longitude = regLongitude }));
+                }
+            }
+            catch
+            {
+                // continue
             }
         }
 
         private async Task GetAllPoints()
         {
-            AllPoints.Add(new Geopoint(SourcePosition));
-
             try
             {
-                string URL = WebURL + string.Format("?source_lat={0}&source_long={1}&dest_lat={2}&dest_long={3}", SourcePosition.Latitude.ToString(), SourcePosition.Longitude.ToString(), DestinationPosition.Latitude.ToString(), DestinationPosition.Longitude.ToString());
-                
-                string json;
-                //string URL = "http://www.google.com";
-                using (var client = new HttpClient())
+                AllPoints.Add(new Geopoint(SourcePosition));
+
+                try
                 {
-                    json = await client.GetStringAsync(URL);
+                    string URL = WebURL + string.Format("?source_lat={0}&source_long={1}&dest_lat={2}&dest_long={3}", SourcePosition.Latitude.ToString(), SourcePosition.Longitude.ToString(), DestinationPosition.Latitude.ToString(), DestinationPosition.Longitude.ToString());
+
+                    string json;
+                    //string URL = "http://www.google.com";
+                    using (var client = new HttpClient())
+                    {
+                        json = await client.GetStringAsync(URL);
+                    }
+
+                    //string json = "{\"1\": [25.2628193, 82.9897208], \"2\": [25.263333, 82.9896565], \"3\": [25.2635136, 82.99269079999999], \"4\": [25.27001, 82.9990535], \"5\": [25.2777705, 83.00220929999999], \"6\": [25.2819148, 83.0030073], \"7\": [25.2880692, 82.9991783], \"8\": [25.2907264, 82.99535639999999], \"9\": [25.2913254, 82.9921354], \"10\": [25.2901792, 82.9899698], \"11\": [25.2898349, 82.98303279999999], \"12\": [25.2892961, 82.96987209999999], \"13\": [25.3019357, 82.96845359999999], \"14\": [25.3003501, 82.9511617], \"15\": [25.2946962, 82.9503714]}";
+
+                    AddJsonPoints(json);
+                }
+                catch (Exception)
+                {
+                    Debug.WriteLine("Nothing Found");
                 }
 
-                //string json = "{\"1\": [25.2628193, 82.9897208], \"2\": [25.263333, 82.9896565], \"3\": [25.2635136, 82.99269079999999], \"4\": [25.27001, 82.9990535], \"5\": [25.2777705, 83.00220929999999], \"6\": [25.2819148, 83.0030073], \"7\": [25.2880692, 82.9991783], \"8\": [25.2907264, 82.99535639999999], \"9\": [25.2913254, 82.9921354], \"10\": [25.2901792, 82.9899698], \"11\": [25.2898349, 82.98303279999999], \"12\": [25.2892961, 82.96987209999999], \"13\": [25.3019357, 82.96845359999999], \"14\": [25.3003501, 82.9511617], \"15\": [25.2946962, 82.9503714]}";
 
-                AddJsonPoints(json);
+                //AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = 25.2628193, Longitude = 82.9897208 }));
+                //AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = 25.2946962, Longitude = 82.9503714 }));
+
+                AllPoints.Add(new Geopoint(DestinationPosition));
             }
-            catch (Exception)
+            catch
             {
-                Debug.WriteLine("Nothing Found");
+                // continue
             }
-
-            //AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = 25.2628193, Longitude = 82.9897208 }));
-            //AllPoints.Add(new Geopoint(new BasicGeoposition() { Latitude = 25.2946962, Longitude = 82.9503714 }));
-
-            AllPoints.Add(new Geopoint(DestinationPosition));
         }
 
         private async Task PlotAllPoints()
         {
-            MapControl1.MapElements.Add(SourceMapIcon);
-            MapControl1.MapElements.Add(DestinationMapIcon);
+            try
+            {
+                MapControl1.MapElements.Add(SourceMapIcon);
+                MapControl1.MapElements.Add(DestinationMapIcon);
 
-            MapRouteFinderResult routeResult =
-                await MapRouteFinder.GetDrivingRouteFromWaypointsAsync(AllPoints);
+                MapRouteFinderResult routeResult =
+                    await MapRouteFinder.GetDrivingRouteFromWaypointsAsync(AllPoints);
 
-            MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
+                MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
 
-            viewOfRoute.RouteColor = Colors.Blue;
-            viewOfRoute.OutlineColor = Colors.Black;
+                viewOfRoute.RouteColor = Colors.Blue;
+                viewOfRoute.OutlineColor = Colors.Black;
 
-            MapControl1.Routes.Add(viewOfRoute);
+                MapControl1.Routes.Add(viewOfRoute);
 
-            ShowTimeBlock.Text = "Time : " + routeResult.Route.EstimatedDuration.ToString();
-            ShowPathBlock.Text = "Distance : " + routeResult.Route.LengthInMeters + "m";
+                ShowTimeBlock.Text = "Time : " + routeResult.Route.EstimatedDuration.ToString();
+                ShowPathBlock.Text = "Distance : " + routeResult.Route.LengthInMeters + "m";
 
-            await MapControl1.TrySetViewBoundsAsync(
-                routeResult.Route.BoundingBox,
-                null,
-                MapAnimationKind.Default);
+                await MapControl1.TrySetViewBoundsAsync(
+                    routeResult.Route.BoundingBox,
+                    null,
+                    MapAnimationKind.Default);
 
-            double angle = angleFromCoordinate(SourcePosition.Latitude, SourcePosition.Longitude,
-                DestinationPosition.Latitude, DestinationPosition.Longitude);
+                double angle = angleFromCoordinate(SourcePosition.Latitude, SourcePosition.Longitude,
+                    DestinationPosition.Latitude, DestinationPosition.Longitude);
 
-            Debug.WriteLine(angle);
-            await MapControl1.TryRotateAsync(-1.0 * angle);
+                Debug.WriteLine(angle);
+                await MapControl1.TryRotateAsync(-1.0 * angle);
 
-            await MapControl1.TryTiltAsync(55);
+                await MapControl1.TryTiltAsync(55);
+            }
+            catch
+            {
+                // continue
+            }
         }
 
         private double angleFromCoordinate(double lat1, double long1, double lat2, double long2)
